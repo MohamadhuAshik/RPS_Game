301292b5069051044a0a2ddfd38a4bd164f32460
MohamadhuAshik <mohamadashik268@gmail.com>
2024-09-24 13:11:54 +0530

    update game

--- RPS_Game.py
+++ RPS_Game.py
@@ -7,32 +7,52 @@ class RPS(Enum):
     PAPER= 2
     SCISSOR = 3
 
-print(" ")
-player_Choise = input("Enter.......\n\n1 for Rock\n2 for paper\n3 for scissor\n\n")
-player = int(player_Choise)
+def play_Game():
+    print(" ")
+    player_Choise = input("Enter.......\n\n1 for Rock\n2 for paper\n3 for scissor\n\n")
+    player = int(player_Choise)
 
-if(player<0 or player>3):
-    sys.exit("You must enter 1,2 or 3")
+    if(player_Choise not in ["1","2","3"]):
+        print("You must enter 1,2 or 3")
+        return play_Game()
 
-computer_Choise = random.choice("123")
-computer = int(computer_Choise)
+    computer_Choise = random.choice("123")
+    computer = int(computer_Choise)
 
 
-print(" ")
+    print(" ")
 
-print("player_Choise :"+" " +str(RPS(player)).replace("RPS.",""))
-print("computer_Choise :"+" " +str(RPS(computer)).replace("RPS.",""))
+    print("player_Choise :"+" " +str(RPS(player)).replace("RPS.",""))
+    print("computer_Choise :"+" " +str(RPS(computer)).replace("RPS.",""))
 
-print(" ")
-if(player==1 and computer==3):
-    print("🎉 You Win!")
-elif(player==2 and computer==1):
-     print("🎉 You Win!")
-elif(player==3 and computer==2):
-     print("🎉 You Win!")
-elif(player == computer):
-    print("😑 tie Game!")
-else:
-    print("🐍 Python Win!")
+    print(" ")
+    if(player==1 and computer==3):
+        print("🎉 You Win!")
+    elif(player==2 and computer==1):
+        print("🎉 You Win!")
+    elif(player==3 and computer==2):
+        print("🎉 You Win!")
+    elif(player == computer):
+        print("😑 tie Game!")
+    else:
+        print("🐍 Python Win!")
 
-print(" ")
+    print("\nPlay Again? ")
+
+    while True:
+        playAgain = input("\nY for yes\nQ for Quit\n\n")
+        if playAgain.lower() not in ["y","q"]:
+            continue
+        else:
+            break
+    
+    
+    if playAgain == "y":
+        return play_Game()
+    else:
+        print("\n🎉🎉🎉")
+        print("Thanks for playing!\n")
+        sys.exit("Bye! 👋")
+        
+
+play_Game()
